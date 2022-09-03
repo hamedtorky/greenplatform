4fda3cdf91684fba4d5618c609e3a76c6723017c
hamed <hamedtorky2@gmail.com>
2022-09-03 18:21:58 +0300

    init

---
+++ scripts/local.bash
@@ -0,0 +1,7 @@
+export DB_URL="postgres"
+export SERVER_IP="127.0.0.1"
+
+echo ${WORK_DIR:-"/code"} ${WORKER_COUNT:-"2"} ${BIND_ADDRESS:-":8000"}
+cd ${WORK_DIR:-"/code"}
+python manage.py collectstatic -c --noinput
+gunicorn bgz_server.wsgi -w ${WORKER_COUNT:-"2"} -b ${BIND_ADDRESS:-":8000"}
